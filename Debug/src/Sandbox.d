e3c94a0b86001791002c3d7b8e8e0f699059dbc4
Roosemberth Palacios <roosemberth@roosemberth.usr.sh>
2014-03-19 21:25:18 -0600

    Simple GaussianBlur Transformation from Camera or File
    Signed-off-by: Roosemberth Palacios <roosemberth@roosemberth.usr.sh>

--- Debug/src/Sandbox.d
+++ Debug/src/Sandbox.d
@@ -1,4 +1,3 @@
-src/Sandbox.d: ../src/Sandbox.cpp \
- /usr/local/include/opencv2/highgui/highgui.hpp
+src/Sandbox.d: ../src/Sandbox.cpp /usr/local/include/opencv2/opencv.hpp
 
-/usr/local/include/opencv2/highgui/highgui.hpp:
+/usr/local/include/opencv2/opencv.hpp:
--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -1,8 +1,19 @@
-#include <highgui/highgui.hpp>
+#include <opencv.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
+
+typedef struct gaussBlurParams{
+	int ksize;
+	int stdX;
+	int stdY;
+	int borderType;
+} GaussBlurParams;
+
+void ProcessFrames(Mat In, Mat Out, GaussBlurParams GBPs){
+	GaussianBlur(In, Out, Size(GBPs.ksize==0?0:2*GBPs.ksize-1, GBPs.ksize==0?0:2*GBPs.ksize-1), GBPs.ksize==0?1:GBPs.stdX, GBPs.ksize==0?1:GBPs.stdY);
+}
 
 int main(int argc, char** argv){
 	VideoCapture Stream;
@@ -15,12 +26,25 @@ int main(int argc, char** argv){
 	} else {
 		Stream.open(argv[1]);		// Open a Video File
 	}
-	Mat frame;
 	namedWindow("Video Stream Viewer", WINDOW_NORMAL);
-	while (waitKey(1)!='c'){
-		Stream >> frame;
-		if (!frame.data) break;
-		imshow("Video Stream Viewer", frame);
+	namedWindow("Output Stream Viewer", WINDOW_NORMAL);
+	Mat FrameD;
+	Stream>>FrameD;
+	Mat *Frame=&FrameD;
+	Mat OutputFrameD;
+	Stream>>OutputFrameD;
+	Mat *OutputFrame=&OutputFrameD;
+	GaussBlurParams DefGBP= {5, 3, 3, 0};
+	GaussBlurParams *GBPs = &DefGBP;
+	while (waitKey(1)!=27){
+		Stream >> *Frame;
+		if (!Frame->data) cout<<"Invalid Frame!"<<endl;
+		imshow("Video Stream Viewer", *Frame);
+		createTrackbar("Kernel Size", "Output Stream Viewer", &GBPs->ksize, 100, NULL);
+		createTrackbar("STD X Sigma", "Output Stream Viewer", &GBPs->stdX, 100, NULL);
+		createTrackbar("STD Y Sigma", "Output Stream Viewer", &GBPs->stdX, 100, NULL);
+		ProcessFrames(*Frame, *OutputFrame, *GBPs);
+		imshow("Output Stream Viewer", *OutputFrame);
 	}
 	cout<<"Good bye cruel world! u.u"<<endl;
 	return 0;

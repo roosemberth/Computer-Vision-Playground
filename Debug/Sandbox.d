819ce02ee6e8ae189ef4f1124fc044b669aad586
Roosembert Palacios <roosembert.palacios@epfl.ch>
2015-12-19 03:48:30 +0100

    clean wd

--- Debug/Sandbox.d
+++
@@ -1,6 +0,0 @@
-Debug/Sandbox.d: src/Sandbox.cpp src/SystechDefinitions.h \
- src/OpenCL-Common.h
-
-src/SystechDefinitions.h:
-
-src/OpenCL-Common.h:
--- Release/Sandbox.d
+++
@@ -1,6 +0,0 @@
-Release/Sandbox.d: src/Sandbox.cpp src/SystechDefinitions.h \
- src/OpenCL-Common.h
-
-src/SystechDefinitions.h:
-
-src/OpenCL-Common.h:
